572aaf3b59831352750fffa6722ca0d1bc025412
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-10-31 21:06:23 +0700

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hello-web3
 Mess up with web3
+
+```
+testrpc
+yarn install
+yarn start
+```
---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import promisify from 'util.promisify'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    // Init
+    this.web3 = new Web3()
+    this.web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'))
+
+    this.state = {
+      loading: true,
+      lucky: 'n/a',
+      transaction: 'n/a'
+    }
+  }
+
+  doAirDrop = async () => {
+    const accounts = this.web3.eth.accounts
+    const sendTransaction = promisify(this.web3.eth.sendTransaction)
+    const to = accounts[Math.floor(accounts.length * Math.random())]
+    const address = await sendTransaction({
+      from: accounts[0],
+      to,
+      value: 10
+    }).catch(console.error)
+
+    this.setState({ lucky: to, transaction: address })
+  }
+
+  componentDidMount = async () => {
+    // Get some props
+    const coinbase = this.web3.eth.coinbase
+    const balance = this.web3.eth.getBalance(coinbase).toString()
+
+    // Watch for change
+    this.web3.eth.filter('latest').watch(() => {
+      const balance = this.web3.eth.getBalance(coinbase).toString()
+      this.setState({ balance })
+    })
+
+    // Send some ether every second
+    setInterval(this.doAirDrop, 1000)
+
+    this.setState({ loading: false, coinbase, balance })
+  }
+
+  showAccount = () =>
+    this.web3.eth.accounts.map((account, index) => (
+      <li key={index}>
+        {account}
+        <ol>{this.web3.eth.getBalance(account).toString()}</ol>
+      </li>
+    ))
+
+  render () {
+    // Loading
+    if (this.state.loading) return <p>loading...</p>
+
+    // Done
+    return (
+      <div>
+        <p>coinbase : {this.state.coinbase}</p>
+        <p>balance : {this.state.balance}</p>
+        <ul>{this.showAccount()}</ul>
+        <p>lucky : {this.state.lucky}</p>
+        <p>transaction : {this.state.transaction}</p>
+      </div>
+    )
+  }
+}
+
+export default App
